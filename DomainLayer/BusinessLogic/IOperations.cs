f21c88cfdda5d46084225a446c6704136a7e6f38
Dwivedi Vishal Sridhar Arya <vidyutron@live.com>
2020-02-01 19:54:32 +0530

    Cleanup and addition of _img folder

--- DomainLayer/BusinessLogic/IOperations.cs
+++ DomainLayer/BusinessLogic/IOperations.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DomainLayer.BusinessLogic
+﻿namespace DomainLayer.BusinessLogic
 {
     public interface IOperations
     {
--- SampleDI/Bootstrap.cs
+++ SampleDI/Bootstrap.cs
@@ -1,12 +1,8 @@
 ﻿using Autofac;
 using DomainLayer.BusinessLogic;
 using SampleDI.AppLogic;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SampleDI
 {
